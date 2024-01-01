eaeb462393d995a0dce891e1891faba0eabf02d7
Jon Evans <jon@craftyjon.com>
2024-01-01 15:41:35 -0500

    Update copyright year

--- examples/tracks.py
+++ examples/tracks.py
@@ -2,7 +2,7 @@
 
 # This program source code file is part of KiCad, a free EDA CAD application.
 #
-# Copyright (C) 2023 KiCad Developers, see AUTHORS.TXT for contributors.
+# Copyright (C) 2024 KiCad Developers
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
